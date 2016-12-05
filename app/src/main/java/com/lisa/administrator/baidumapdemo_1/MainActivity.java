a8394a97b0a7ccb6c7cacb8c79d0b3def5570f99
秦莉莎 <980004531@qq.com>
2016-12-05 09:50:45 +0800

    frist

---
+++ app/src/main/java/com/lisa/administrator/baidumapdemo_1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.lisa.administrator.baidumapdemo_1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    MapView mMapView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mMapView = (MapView) findViewById(R.id.bmapView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+}
